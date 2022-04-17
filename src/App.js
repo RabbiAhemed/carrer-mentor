5d29c94d8432b4ef8df3a3395bed7b2f87d162bb
Rabbi Ahmed Limon <rabbiahemed>
2022-04-18 00:03:00 +0600

    Not Found Added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home'
 import Footer from './Pages/Footer/Footer';
 import Header from './Pages/Header/Header';
+import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
+import NotFound from './Pages/NotFound/NotFound';
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
